b6bc2761001f3bc48ace889cff5ec7e669a97ddd
seravifer <sergiavilafernandez@gmail.com>
2016-10-18 20:40:45 +0200

    Template

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
-            $(window).scroll(function () {
-
-                if ($(window).scrollTop() > 10) {
-                    $(".navbar.scroll").addClass("move");
-                } else {
-                    $(".navbar.scroll.move").removeClass("move");
-                }
-            });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 10) {
+            $(".navbar.scroll").addClass("move");
+        } else {
+            $(".navbar.scroll.move").removeClass("move");
+        }
+    });
 });
